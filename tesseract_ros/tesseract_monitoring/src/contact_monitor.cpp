2b8a8990c11b595f1cd926678c4cbc9cb1064fee
Joseph Schornak <joe.schornak@gmail.com>
2019-07-19 16:59:58 -0500

    Port contact_monitor.cpp

--- tesseract_ros/tesseract_monitoring/src/contact_monitor.cpp
+++ tesseract_ros/tesseract_monitoring/src/contact_monitor.cpp
@@ -1,12 +1,14 @@
 #include <tesseract_common/macros.h>
 TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
 #include <pluginlib/class_loader.hpp>
-#include <ros/ros.h>
-#include <ros/package.h>
-#include <sensor_msgs/JointState.h>
-#include <tesseract_msgs/ContactResultVector.h>
-#include <tesseract_msgs/ModifyEnvironment.h>
-#include <tesseract_msgs/ComputeContactResultVector.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include <tesseract_msgs/msg/contact_result_vector.hpp>
+#include <tesseract_msgs/srv/modify_environment.hpp>
+#include <tesseract_msgs/srv/compute_contact_result_vector.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/shared_mutex.hpp>
+#include <boost/thread/recursive_mutex.hpp>
 TESSERACT_COMMON_IGNORE_WARNINGS_POP
 
 #include <tesseract_collision/core/discrete_contact_manager.h>
@@ -18,6 +20,8 @@ TESSERACT_COMMON_IGNORE_WARNINGS_POP
 #include <tesseract_environment/kdl/kdl_env.h>
 #include <tesseract_environment/core/utils.h>
 #include <tesseract_rosutils/utils.h>
+
+// TODO: Reimplement all this as a class with member functions
 
 using namespace tesseract;
 using namespace tesseract_environment;
@@ -34,20 +38,23 @@ const double DEFAULT_CONTACT_DISTANCE = 0.1;
 
 static Tesseract::Ptr tess;
 static DiscreteContactManager::Ptr manager;
-static ros::Subscriber joint_states_sub;
-static ros::Publisher contact_results_pub;
-static ros::Publisher environment_pub;
-static ros::Subscriber environment_diff_sub;
-static ros::ServiceServer modify_env_service;
-static ros::ServiceServer compute_contact_results;
+
+static rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_states_sub;
+static rclcpp::Publisher<tesseract_msgs::msg::ContactResultVector>::SharedPtr contact_results_pub;
+static rclcpp::Publisher<tesseract_msgs::msg::TesseractState>::SharedPtr environment_pub;
+static rclcpp::Subscription<tesseract_msgs::msg::TesseractState>::SharedPtr environment_diff_sub;
+
+static rclcpp::Service<tesseract_msgs::srv::ModifyEnvironment>::SharedPtr modify_env_service;
+static rclcpp::Service<tesseract_msgs::srv::ComputeContactResultVector>::SharedPtr compute_contact_results;
+
 static ContactTestType type;
 static ContactResultMap contacts;
-static tesseract_msgs::ContactResultVector contacts_msg;
+static tesseract_msgs::msg::ContactResultVector contacts_msg;
 static bool publish_environment;
 static boost::mutex modify_mutex;
 static DiscreteContactManagerPluginLoaderPtr discrete_manager_loader; /**< The discrete contact manager loader */
 
-void callbackJointState(const sensor_msgs::JointState::ConstPtr& msg)
+void callbackJointState(const sensor_msgs::msg::JointState::SharedPtr msg)
 {
   boost::mutex::scoped_lock(modify_mutex);
   contacts.clear();
@@ -61,9 +68,9 @@ void callbackJointState(const sensor_msgs::JointState::ConstPtr& msg)
 
   if (publish_environment)
   {
-    tesseract_msgs::TesseractState state_msg;
+    tesseract_msgs::msg::TesseractState state_msg;
     toMsg(state_msg, *(tess->getEnvironment()));
-    environment_pub.publish(state_msg);
+    environment_pub->publish(state_msg);
   }
 
   ContactResultVector contacts_vector;
@@ -71,19 +78,19 @@ void callbackJointState(const sensor_msgs::JointState::ConstPtr& msg)
   contacts_msg.contacts.reserve(contacts_vector.size());
   for (const auto& contact : contacts_vector)
   {
-    tesseract_msgs::ContactResult contact_msg;
-    toMsg(contact_msg, contact, msg->header.stamp);
+    tesseract_msgs::msg::ContactResult contact_msg;
+    toMsg(contact_msg, contact);
     contacts_msg.contacts.push_back(contact_msg);
   }
-  contact_results_pub.publish(contacts_msg);
+  contact_results_pub->publish(contacts_msg);
 }
 
-bool callbackModifyTesseractEnv(tesseract_msgs::ModifyEnvironmentRequest& request,
-                                tesseract_msgs::ModifyEnvironmentResponse& response)
+void callbackModifyTesseractEnv(const tesseract_msgs::srv::ModifyEnvironment::Request::SharedPtr request,
+                                tesseract_msgs::srv::ModifyEnvironment::Response::SharedPtr response)
 {
   boost::mutex::scoped_lock(modify_mutex);
-  response.success = processMsg(*(tess->getEnvironment()), request.commands);
-  response.revision = tess->getEnvironmentConst()->getRevision();
+  response->success = processMsg(*(tess->getEnvironment()), request->commands);
+  response->revision = tess->getEnvironmentConst()->getRevision();
 
   // Create a new manager
   std::vector<std::string> active = manager->getActiveCollisionObjects();
@@ -95,17 +102,17 @@ bool callbackModifyTesseractEnv(tesseract_msgs::ModifyEnvironmentRequest& reques
   manager->setContactDistanceThreshold(contact_distance);
   manager->setIsContactAllowedFn(fn);
 
-  return true;
+  return;
 }
 
-bool callbackComputeContactResultVector(tesseract_msgs::ComputeContactResultVectorRequest& request,
-                                        tesseract_msgs::ComputeContactResultVectorResponse& response)
+void callbackComputeContactResultVector(const tesseract_msgs::srv::ComputeContactResultVector::Request::SharedPtr request,
+                                        tesseract_msgs::srv::ComputeContactResultVector::Response::SharedPtr response)
 {
   ContactResultMap contacts;
 
   boost::mutex::scoped_lock(modify_mutex);
 
-  tess->getEnvironment()->setState(request.joint_states.name, request.joint_states.position);
+  tess->getEnvironment()->setState(request->joint_states.name, request->joint_states.position);
   EnvState::ConstPtr state = tess->getEnvironment()->getCurrentState();
 
   manager->setCollisionObjectsTransform(state->transforms);
@@ -113,19 +120,19 @@ bool callbackComputeContactResultVector(tesseract_msgs::ComputeContactResultVect
 
   ContactResultVector contacts_vector;
   tesseract_collision::flattenResults(std::move(contacts), contacts_vector);
-  response.collision_result.contacts.reserve(contacts_vector.size());
+  response->collision_result.contacts.reserve(contacts_vector.size());
   for (const auto& contact : contacts_vector)
   {
-    tesseract_msgs::ContactResult contact_msg;
-    toMsg(contact_msg, contact, request.joint_states.header.stamp);
-    response.collision_result.contacts.push_back(contact_msg);
+    tesseract_msgs::msg::ContactResult contact_msg;
+    toMsg(contact_msg, contact);
+    response->collision_result.contacts.push_back(contact_msg);
   }
-  response.success = true;
+  response->success = true;
 
-  return true;
+  return;
 }
 
-void callbackTesseractEnvDiff(const tesseract_msgs::TesseractStatePtr& state)
+void callbackTesseractEnvDiff(const tesseract_msgs::msg::TesseractState::SharedPtr state)
 {
   //  if (!state->is_diff)
   //    return;
@@ -133,7 +140,7 @@ void callbackTesseractEnvDiff(const tesseract_msgs::TesseractStatePtr& state)
   boost::mutex::scoped_lock(modify_mutex);
   if (!processMsg(*(tess->getEnvironment()), *state))
   {
-    ROS_ERROR("Invalid TesseractState diff message");
+//    ROS_ERROR("Invalid TesseractState diff message");  // TODO: re-enable
   }
 
   // Create a new manager
@@ -149,17 +156,24 @@ void callbackTesseractEnvDiff(const tesseract_msgs::TesseractStatePtr& state)
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "tesseract_contact_monitoring");
-  ros::NodeHandle nh;
-  ros::NodeHandle pnh("~");
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("tesseract_contact_monitoring");
 
   SceneGraph::Ptr scene_graph;
   SRDFModel::Ptr srdf_model;
   std::string robot_description;
   std::string plugin;
 
-  pnh.param<std::string>("robot_description", robot_description, ROBOT_DESCRIPTION_PARAM);
-  pnh.param<bool>("publish_environment", publish_environment, false);
+  node->declare_parameter("robot_description");
+  node->declare_parameter("publish_environment");
+
+  node->get_parameter_or("robot_description", robot_description, ROBOT_DESCRIPTION_PARAM);
+  node->get_parameter_or("publish_environment", publish_environment, false);
+
+
+  node->get_parameter_or("robot_description", robot_description, ROBOT_DESCRIPTION_PARAM);
+  node->get_parameter_or("publish_environment", publish_environment, false);
+
   //  if (pnh.hasParam("plugin"))
   //  {
   //    discrete_manager_loader.reset(new DiscreteContactManagerPluginLoader("tesseract_collision",
@@ -175,26 +189,26 @@ int main(int argc, char** argv)
 
   // Initial setup
   std::string urdf_xml_string, srdf_xml_string;
-  if (!nh.hasParam(robot_description))
+  if (!node->has_parameter(robot_description))
   {
-    ROS_ERROR("Failed to find parameter: %s", robot_description.c_str());
+    RCLCPP_ERROR(node->get_logger(), "Failed to find parameter: %s", robot_description.c_str());
     return -1;
   }
 
-  if (!nh.hasParam(robot_description + "_semantic"))
+  if (!node->has_parameter(robot_description + "_semantic"))
   {
-    ROS_ERROR("Failed to find parameter: %s", (robot_description + "_semantic").c_str());
+    RCLCPP_ERROR(node->get_logger(), "Failed to find parameter: %s", (robot_description + "_semantic").c_str());
     return -1;
   }
 
-  nh.getParam(robot_description, urdf_xml_string);
-  nh.getParam(robot_description + "_semantic", srdf_xml_string);
+  node->get_parameter(robot_description, urdf_xml_string);
+  node->get_parameter(robot_description + "_semantic", srdf_xml_string);
 
   tess = std::make_shared<tesseract::Tesseract>();
   ResourceLocatorFn locator = tesseract_rosutils::locateResource;
   if (!tess->init(urdf_xml_string, srdf_xml_string, locator))
   {
-    ROS_ERROR("Failed to initialize environment.");
+    RCLCPP_ERROR(node->get_logger(), "Failed to initialize environment.");
     return -1;
   }
 
@@ -202,22 +216,22 @@ int main(int argc, char** argv)
   std::vector<std::string> link_names;
   double contact_distance;
 
-  pnh.param<double>("contact_distance", contact_distance, DEFAULT_CONTACT_DISTANCE);
+  node->get_parameter_or("contact_distance", contact_distance, DEFAULT_CONTACT_DISTANCE);
 
   link_names = tess->getEnvironment()->getLinkNames();
-  if (pnh.hasParam("monitor_links"))
-    pnh.getParam("monitor_links", link_names);
+  if (node->has_parameter("monitor_links"))
+    node->get_parameter("monitor_links", link_names);
 
   if (link_names.empty())
     link_names = tess->getEnvironment()->getLinkNames();
 
   int contact_test_type = 2;
-  if (pnh.hasParam("contact_test_type"))
-    pnh.getParam("contact_test_type", contact_test_type);
+  if (node->has_parameter("contact_test_type"))
+    node->get_parameter("contact_test_type", contact_test_type);
 
   if (contact_test_type < 0 || contact_test_type > 3)
   {
-    ROS_WARN("Request type must be 0, 1, 2 or 3. Setting to 2(ALL)!");
+    RCLCPP_WARN(node->get_logger(), "Request type must be 0, 1, 2 or 3. Setting to 2(ALL)!");
     contact_test_type = 2;
   }
   type = static_cast<ContactTestType>(contact_test_type);
@@ -226,24 +240,21 @@ int main(int argc, char** argv)
   manager->setActiveCollisionObjects(link_names);
   manager->setContactDistanceThreshold(contact_distance);
 
-  joint_states_sub = nh.subscribe("joint_states", 1, &callbackJointState);
-  contact_results_pub = pnh.advertise<tesseract_msgs::ContactResultVector>("contact_results", 1, true);
-  modify_env_service =
-      pnh.advertiseService<tesseract_msgs::ModifyEnvironmentRequest, tesseract_msgs::ModifyEnvironmentResponse>(
-          "modify_environment", &callbackModifyTesseractEnv);
+  joint_states_sub = node->create_subscription<sensor_msgs::msg::JointState>("joint_states", 1, &callbackJointState);
+  contact_results_pub = node->create_publisher<tesseract_msgs::msg::ContactResultVector>("contact_results", 1);
+  modify_env_service = node->create_service<tesseract_msgs::srv::ModifyEnvironment>("modify_environment", &callbackModifyTesseractEnv);
 
   if (publish_environment)
-    environment_pub = pnh.advertise<tesseract_msgs::TesseractState>("tesseract", 100, false);
+    environment_pub = node->create_publisher<tesseract_msgs::msg::TesseractState>("tesseract", 100);
 
-  compute_contact_results = pnh.advertiseService<tesseract_msgs::ComputeContactResultVectorRequest,
-                                                 tesseract_msgs::ComputeContactResultVectorResponse>(
-      "compute_contact_results", &callbackComputeContactResultVector);
+  compute_contact_results = node->create_service<tesseract_msgs::srv::ComputeContactResultVector>("compute_contact_results", &callbackComputeContactResultVector);
 
-  environment_diff_sub = pnh.subscribe("tesseract_diff", 100, &callbackTesseractEnvDiff);
+  environment_diff_sub = node->create_subscription<tesseract_msgs::msg::TesseractState>("tesseract_diff", 100, &callbackTesseractEnvDiff);
 
-  ROS_INFO("Contact Monitor Running!");
+  RCLCPP_INFO(node->get_logger(), "Contact Monitor Running!");
 
-  ros::spin();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
 
   return 0;
 }
